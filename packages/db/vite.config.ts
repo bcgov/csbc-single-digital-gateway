dcb991176c37a4b9d7879d7348f30ae13b26c400
Sid McLaughlin <sid.mclaughlin@gov.bc.ca>
2026-03-12 08:59:34 -0700

    use vite to build, output cjs/es

---
+++ packages/db/vite.config.ts
@@ -0,0 +1,19 @@
+import path from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      formats: ["es", "cjs"],
+      fileName: (format) => (format === "es" ? "index.js" : "index.cjs"),
+    },
+  },
+  plugins: [
+    dts({
+      include: ["src/**/*"],
+      tsconfigPath: "./tsconfig.build.json",
+    }),
+  ],
+});
